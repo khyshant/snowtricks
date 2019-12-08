5bd5971d843769fa1658158e885caea06c80744c
khyshant <anth.blanchard@gmail.com>
2019-12-08 16:11:16 +0100

    Update Trick.php
    add property without relation

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TrickRepository")
@@ -16,12 +17,49 @@ class Trick
      */
     private $id;
 
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", lenght="255" )
+     * @Assert\NotBlank
+     */
     private $title;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="text")
+     */
     private $description;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", lenght="255" )
+     */
     private $metatitle;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="text")
+     */
     private $metadescription;
+
+    /**
+     * @var boolean|null
+     * @ORM\Column(type="boolean")
+     */
     private $is_valid;
+
+    /**
+     * @var string
+     * @var string A "Y-m-d H:i:s" formatted value
+     * @Assert\DateTime()
+     */
     private $date_add;
+
+    /**
+     * @var string
+     * @var string A "Y-m-d H:i:s" formatted value
+     * @Assert\DateTime()
+     */
     private $date_update;
     /*__________relations___________*/
     private $author;
