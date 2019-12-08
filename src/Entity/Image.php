1e2e919c415bcfda68fa42c937fd07db18b26be0
khyshant <anth.blanchard@gmail.com>
2019-12-08 21:54:09 +0100

    update
    update image,
    add migration

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -26,23 +26,71 @@ class Image
     private $path;
 
     /**
+     * @var UploadedFile|null
+     * @Assert\Image
+     * @Assert\NotNull(groups={"add"})
+     */
+    private $uploadedFile;
+    /*__________relations___________*/
+    /**
      * @var Trick|null
      * @ORM\ManyToOne(targetEntity="Trick", inversedBy="images")
      * @ORM\JoinColumn(onDelete="CASCADE")
      */
     private $trick;
 
-    /**
-     * @var UploadedFile|null
-     * @Assert\Image
-     * @Assert\NotNull(groups={"add"})
-     */
-    private $uploadedFile;
-
-
+    /*__________getter and setter___________*/
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param null|string $path
+     */
+    public function setPath(?string $path): void
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * @return null|UploadedFile
+     */
+    public function getUploadedFile(): ?UploadedFile
+    {
+        return $this->uploadedFile;
+    }
+
+    /**
+     * @param null|UploadedFile $uploadedFile
+     */
+    public function setUploadedFile(?UploadedFile $uploadedFile): void
+    {
+        $this->uploadedFile = $uploadedFile;
+    }
+
+    /**
+     * @return Trick|null
+     */
+    public function getTrick(): ?Trick
+    {
+        return $this->trick;
+    }
+
+    /**
+     * @param Trick|null $trick
+     */
+    public function setTrick(?Trick $trick): void
+    {
+        $this->trick = $trick;
     }
 
 
--- src/Migrations/Version20191208185956.php
+++ src/Migrations/Version20191208185956.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191208151725 extends AbstractMigration
+final class Version20191208185956 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -24,10 +24,11 @@ final class Version20191208151725 extends AbstractMigration
 
         $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE group_trick (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, trick_id INT DEFAULT NULL, path VARCHAR(255) NOT NULL, INDEX IDX_C53D045FB281BE2E (trick_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE trick (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, metatitle VARCHAR(255) NOT NULL, metadescription LONGTEXT NOT NULL, is_valid TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE video (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE image ADD CONSTRAINT FK_C53D045FB281BE2E FOREIGN KEY (trick_id) REFERENCES trick (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema) : void
@@ -35,6 +36,7 @@ final class Version20191208151725 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE image DROP FOREIGN KEY FK_C53D045FB281BE2E');
         $this->addSql('DROP TABLE comment');
         $this->addSql('DROP TABLE group_trick');
         $this->addSql('DROP TABLE image');
