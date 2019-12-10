c20c006b44a8f05edd4a394506f3d86ad255b762
khyshant <anth.blanchard@gmail.com>
2019-12-10 16:20:42 +0100

    add comment entity

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -16,8 +16,48 @@ class Comment
      */
     private $id;
 
+    /**
+     * @var string|null
+     * @ORM\Column(type="text")
+     */
+    private $comment;
+
+    /**
+     * @var string
+     * @var string A "Y-m-d H:i:s" formatted value
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $date_add;
+
+    /**
+     * @var string
+     * @var string A "Y-m-d H:i:s" formatted value
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $date_valid;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Trick", inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $trick;
+
+    private $author;
+
+    /*__________construc___________*/
+    public function __construct(){
+
+    }
+
+    /*__________getter and setter___________*/
+
     public function getId(): ?int
     {
         return $this->id;
     }
+
+
+
+
+
 }
