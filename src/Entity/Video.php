34115e719f047689aa2192ffb00689730aecfec4
khyshant <anth.blanchard@gmail.com>
2019-12-27 13:27:03 +0100

    Update Video.php

--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\VideoRepository")
@@ -20,6 +21,7 @@ class Video
 
     /**
      * @var string|null
+     * @Assert\Url
      * @ORM\Column
      */
     private $uri;
